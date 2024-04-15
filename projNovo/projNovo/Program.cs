524c3f24fadc7cc4c910d1a47e614eac53e0ea24
mmarques-ssz <37246763+mmarques-ssz@users.noreply.github.com>
2024-04-15 20:11:31 -0300

    Add files via upload

---
+++ projNovo/projNovo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projNovo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Este é um projeto novo");
+        }
+    }
+}
